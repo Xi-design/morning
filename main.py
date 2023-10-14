8c894eb4a934ec3b4fc5ad9d47462240ebeaacd4
Xi-design <68377812+Xi-design@users.noreply.github.com>
2023-10-14 21:12:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import os
 import random
 
 today = datetime.now()
-start_date = os.environ['START_DATE']
+#start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
+key = os.environ['WEA_KEY']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
@@ -18,15 +19,22 @@ user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
+# def get_weather():
+#   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+#   res = requests.get(url).json()
+#   weather = res['data']['list'][0]
+#   return weather['weather'], math.floor(weather['temp'])
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    url = "https://devapi.qweather.com/v7/weather/3d?location=101190113&lang=zh&unit=m&key=" + key
+    res = requests.get(url).json()
+    weather_today = res['daily'][0]
+    weather_tomorrow = res['daily'][1]
+    weather_after_tomorrow = res['daily'][2]
+    return weather_today,weather_tomorrow,weather_after_tomorrow,res['fxLink']
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+# def get_count():
+#   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+#   return delta.days
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
@@ -47,7 +55,8 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+weather_today,weather_tomorrow,weather_after_tomorrow,linkdetail = get_weather()
+# data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":weather_today['textDay']},"MaxTemp":{"value":weather_today['tempMax']},"MinTemp":{"value":weather_today['tempMin']},"Featurewea":{"value":weather_tomorrow['fxDate'] + ":" +weather_tomorrow['textDay'] + ";" + weather_after_tomorrow['fxDate'] + ":" + weather_after_tomorrow['textDay']},"Featuretemp":{"value":weather_tomorrow['fxDate'] + ":" +weather_tomorrow['tempMax'] + "°C;" + weather_after_tomorrow['fxDate'] + ":" + weather_after_tomorrow['tempMax']+"°C."},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(),"color":get_random_color()},"links":{"value":linkdetail}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
