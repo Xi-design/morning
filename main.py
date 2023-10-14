8a9b5e86bd54e616ebabb000b5eda77b8b17abbc
Xi-design <68377812+Xi-design@users.noreply.github.com>
2023-10-14 21:16:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.now()
 #start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-key = os.environ['WEA_KEY']
+key = os.environ["WEA_KEY"]
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
